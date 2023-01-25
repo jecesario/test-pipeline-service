181027f13cc8f15fbfe134a96ba5becc2fd13f66
jecesario <jean.cesario-ext@ab-inbev.com>
2023-01-24 21:06:46 -0300

    first commit

---
+++ src/main/java/com/example/testpipelineservice/TestPipelineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpipelineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPipelineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPipelineServiceApplication.class, args);
+    }
+
+}
